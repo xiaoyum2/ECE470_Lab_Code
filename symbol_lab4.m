b5eff572553a54c52e88b44d4ddeb6ef04b93380
xiaoyum2 <61123854+xiaoyum2@users.noreply.github.com>
2020-03-30 18:23:01 -0500

    Add files via upload

---
+++ symbol_lab4.m
@@ -0,0 +1,7 @@
+syms theta1 theta2 theta3 theta4 theta5 theta6;
+T1 = exp_screw_theta(w1,v1,theta1);
+T2 = exp_screw_theta(w2,v2,theta2);
+T3 = exp_screw_theta(w3,v3,theta3);
+T4 = exp_screw_theta(w4,v4,theta4);
+T5 = exp_screw_theta(w5,w5,theta5);
+T6 = exp_screw_theta(w6,w6,theta6)
